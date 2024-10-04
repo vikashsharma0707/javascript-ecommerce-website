e39ee07d924e0d8787b67bab77d6e4c25f542418
vikash sharma <vikashtiwari9893@gmail.com>
2024-10-05 01:40:13 +0530

    navbar checkbox and account onclick function in javascript

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ let searchForm= document.querySelector(".search-form")
 document.querySelector("#search-btn").onclick =()=>{
     searchForm.classList.toggle("active");
 }
+
+let shoppingCart= document.querySelector(".shopping-cart")
+document.querySelector("#cart-btn").onclick =()=>{
+    shoppingCart.classList.toggle("active");
+}
+
+let Account= document.querySelector(".login")
+document.querySelector("#login-btn").onclick =()=>{
+    Account.classList.toggle("active");
+}
+
+
+let Account1= document.querySelector(".login")
+document.querySelector("#login-btn1").onclick =()=>{
+    Account1.classList.toggle("active1");
+}
